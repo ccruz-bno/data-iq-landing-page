876b9ba43b007fffa35ac618300a0c5c185b08ee
Carlos Cruz <ccruz@black-n-orange.com>
2026-04-27 18:21:20 -0600

    Add files via upload
    Actualización de CSS

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        navy: {
+          dark: '#0F206C',
+          DEFAULT: '#131E48',
+          light: '#5D2DE6'
+        },
+        cyan: {
+          DEFAULT: '#00C1DE',
+          light: '#00E2FF'
+        },
+        pink: {
+          DEFAULT: '#FF7F96'
+        },
+        background: {
+          DEFAULT: '#FAFAFA'
+        }
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      }
+    }
+  }
+}
